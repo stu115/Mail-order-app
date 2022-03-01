6871861b71ad94f0facb7d1c2a7961c972d2bf1c
Stefan Ahmed <32064735+stu115@users.noreply.github.com>
2022-03-01 22:27:41 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+} from 'react-native';
+import Pages from './components/Pages';
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Pages/>
+    );
+  }
+}
